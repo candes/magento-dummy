fc05cd939a0fdd4c1f4f9d0c657d4262b10d6c97
Cristian Neagu <cristian.neagu@evozon.com>
2014-02-08 23:55:56 +0200

    update build

--- vendor/theseer/directoryscanner/pear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -f Directory-Scanner*.tgz
-mkdir -p TheSeer/DirectoryScanner
-cp -r src/* TheSeer/DirectoryScanner
-cp dist.php TheSeer/DirectoryScanner/autoload.php
-pear package
-rm -rf TheSeer
